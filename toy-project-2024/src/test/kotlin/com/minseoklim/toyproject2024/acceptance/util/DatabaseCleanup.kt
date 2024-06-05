89969eb7eade4f517b7194d7f072a8d143e97a0b
minseoklim <ddalgu2@gmail.com>
2024-06-05 23:17:56 +0900

    test(toy-project-2024): 인수 테스트 유틸 클래스 추가

---
+++ toy-project-2024/src/test/kotlin/com/minseoklim/toyproject2024/acceptance/util/DatabaseCleanup.kt
@@ -0,0 +1,34 @@
+package com.minseoklim.toyproject2024.acceptance.util
+
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
+import org.springframework.stereotype.Service
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.transaction.annotation.Transactional
+import javax.sql.DataSource
+
+@Service
+@ActiveProfiles("test")
+class DatabaseCleanup(
+    dataSource: DataSource
+) {
+    private val jdbcTemplate = NamedParameterJdbcTemplate(dataSource)
+
+    @Transactional
+    fun execute() {
+        val tableNames = extractTableNames()
+
+        execute("SET REFERENTIAL_INTEGRITY FALSE")
+        tableNames.forEach {
+            execute("TRUNCATE TABLE $it")
+        }
+        execute("SET REFERENTIAL_INTEGRITY TRUE")
+    }
+
+    private fun extractTableNames(): List<String> {
+        return jdbcTemplate.query("SHOW TABLES") { resultSet, _ -> resultSet.getString(1) }
+    }
+
+    private fun execute(query: String) {
+        jdbcTemplate.update(query, emptyMap<String, Any>())
+    }
+}
---
+++ toy-project-2024/src/test/kotlin/com/minseoklim/toyproject2024/acceptance/util/RequestUtil.kt
@@ -0,0 +1,97 @@
+package com.minseoklim.toyproject2024.acceptance.util
+
+import io.restassured.RestAssured
+import io.restassured.http.ContentType
+import io.restassured.response.ExtractableResponse
+import io.restassured.response.Response
+
+object RequestUtil {
+    fun get(
+        path: String,
+        accessToken: String?,
+        vararg pathParams: Any
+    ): ExtractableResponse<Response> {
+        return RestAssured
+            .given().log().all()
+            .apply {
+                if (accessToken != null) {
+                    auth().oauth2(accessToken)
+                }
+            }
+            .`when`().get(path, *pathParams)
+            .then().log().all().extract()
+    }
+
+    fun post(
+        path: String,
+        accessToken: String?,
+        bodyParam: Map<String, Any?>,
+        vararg pathParams: Any
+    ): ExtractableResponse<Response> {
+        return RestAssured
+            .given().log().all()
+            .apply {
+                if (accessToken != null) {
+                    auth().oauth2(accessToken)
+                }
+            }
+            .body(bodyParam)
+            .contentType(ContentType.JSON)
+            .`when`().post(path, *pathParams)
+            .then().log().all().extract()
+    }
+
+    fun put(
+        path: String,
+        accessToken: String?,
+        bodyParam: Map<String, Any?>,
+        vararg pathParams: Any
+    ): ExtractableResponse<Response> {
+        return RestAssured
+            .given().log().all()
+            .apply {
+                if (accessToken != null) {
+                    auth().oauth2(accessToken)
+                }
+            }
+            .body(bodyParam)
+            .contentType(ContentType.JSON)
+            .`when`().put(path, *pathParams)
+            .then().log().all().extract()
+    }
+
+    fun patch(
+        path: String,
+        accessToken: String?,
+        bodyParam: Map<String, Any?>,
+        vararg pathParams: Any
+    ): ExtractableResponse<Response> {
+        return RestAssured
+            .given().log().all()
+            .apply {
+                if (accessToken != null) {
+                    auth().oauth2(accessToken)
+                }
+            }
+            .body(bodyParam)
+            .contentType(ContentType.JSON)
+            .`when`().patch(path, *pathParams)
+            .then().log().all().extract()
+    }
+
+    fun delete(
+        path: String,
+        accessToken: String?,
+        vararg pathParams: Any
+    ): ExtractableResponse<Response> {
+        return RestAssured
+            .given().log().all()
+            .apply {
+                if (accessToken != null) {
+                    auth().oauth2(accessToken)
+                }
+            }
+            .`when`().delete(path, *pathParams)
+            .then().log().all().extract()
+    }
+}
