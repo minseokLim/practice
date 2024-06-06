1a317fda459120b7a1e8d5c713161675cb344bfc
minseoklim <ddalgu2@gmail.com>
2024-06-06 17:30:50 +0900

    feat(toy-project-2024:common): JpaAuditing을 이용한 BaseTimeEntity 추가

---
+++ toy-project-2024/src/main/kotlin/com/minseoklim/toyproject2024/common/config/JpaConfig.kt
@@ -0,0 +1,8 @@
+package com.minseoklim.toyproject2024.common.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@Configuration
+@EnableJpaAuditing
+class JpaConfig
