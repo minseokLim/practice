3050fa1d82ac7ba603a81f7f5f517b0bb150616a
minseoklim <ddalgu2@gmail.com>
2024-06-07 21:00:37 +0900

    feat(toy-project-2024): yaml 파일 로더 생성

---
+++ toy-project-2024/auth-module/src/main/kotlin/com/minseoklim/toyproject2024/auth/config/AuthEnvironmentPostProcessor.kt
@@ -0,0 +1,5 @@
+package com.minseoklim.toyproject2024.auth.config
+
+import com.minseoklim.toyproject2024.common.config.CustomEnvironmentPostProcessor
+
+class AuthEnvironmentPostProcessor : CustomEnvironmentPostProcessor("auth")
---
+++ toy-project-2024/common-module/src/main/kotlin/com/minseoklim/toyproject2024/common/config/CustomEnvironmentPostProcessor.kt
@@ -0,0 +1,40 @@
+package com.minseoklim.toyproject2024.common.config
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.env.EnvironmentPostProcessor
+import org.springframework.boot.env.YamlPropertySourceLoader
+import org.springframework.core.env.CompositePropertySource
+import org.springframework.core.env.ConfigurableEnvironment
+import org.springframework.core.env.PropertySource
+import org.springframework.core.io.ClassPathResource
+
+abstract class CustomEnvironmentPostProcessor(
+    private val propertyName: String
+) : EnvironmentPostProcessor {
+    private val yamlLoader = YamlPropertySourceLoader()
+
+    override fun postProcessEnvironment(environment: ConfigurableEnvironment, application: SpringApplication) {
+        val propertySource = load(environment.activeProfiles)
+        environment.propertySources.addLast(propertySource)
+    }
+
+    private fun load(profiles: Array<String>): PropertySource<Any> {
+        return CompositePropertySource(propertyName).apply {
+            addFirstPropertySource(getYmlProperty("$propertyName.yml")!!)
+
+            profiles.forEach { profile ->
+                getYmlProperty("$propertyName-$profile.yml")?.let {
+                    addFirstPropertySource(it)
+                }
+            }
+        }
+    }
+
+    private fun getYmlProperty(fileName: String): PropertySource<*>? {
+        return try {
+            yamlLoader.load(fileName, ClassPathResource(fileName))[0]
+        } catch (e: IllegalStateException) {
+            null
+        }
+    }
+}
