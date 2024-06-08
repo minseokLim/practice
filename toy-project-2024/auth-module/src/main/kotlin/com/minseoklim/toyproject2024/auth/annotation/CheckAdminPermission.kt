55e64f4b6905e04b2f5080995d2064ff1575ee83
minseoklim <ddalgu2@gmail.com>
2024-06-08 22:03:01 +0900

    feat(toy-project-2024: auth): ADMIN 권한을 체크하는 어노테이션 생성

---
+++ toy-project-2024/auth-module/src/main/kotlin/com/minseoklim/toyproject2024/auth/annotation/CheckAdminPermission.kt
@@ -0,0 +1,8 @@
+package com.minseoklim.toyproject2024.auth.annotation
+
+import org.springframework.security.access.prepost.PreAuthorize
+
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+@PreAuthorize("hasRole('ADMIN')")
+annotation class CheckAdminPermission
