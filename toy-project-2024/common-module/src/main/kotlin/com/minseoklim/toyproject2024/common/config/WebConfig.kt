bd99327633042817bfed1a8549d7a7b015806e16
minseoklim <ddalgu2@gmail.com>
2024-06-10 07:11:27 +0900

    feat(toy-project-2024): pageSerializationMode = VIA_DTO로 설정하여 PageImpl 객체가 PagedModel로 wrapping되도록 설정

--- toy-project-2024/common-module/src/main/kotlin/com/minseoklim/toyproject2024/common/config/WebConfig.kt
+++ toy-project-2024/common-module/src/main/kotlin/com/minseoklim/toyproject2024/common/config/WebConfig.kt
@@ -1,11 +1,14 @@
 package com.minseoklim.toyproject2024.common.config
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.web.config.EnableSpringDataWebSupport
+import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO
 import org.springframework.http.HttpStatus
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 class WebConfig : WebMvcConfigurer {
     override fun addViewControllers(registry: ViewControllerRegistry) {
         registry.addStatusController("/", HttpStatus.OK)
