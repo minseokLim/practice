dab2f4a285a22b65708f52c4531c9a9ab0dc2396
minseoklim <ddalgu2@gmail.com>
2024-06-26 15:00:29 +0900

    feat(toy-project-2024-frontend): 로그인과 회원 가입 화면을 백엔드 서버로 옮김

--- toy-project-2024/frontend/src/main/kotlin/com/minseoklim/toyproject2024/ui/ViewController.kt
+++ toy-project-2024/frontend/src/main/kotlin/com/minseoklim/toyproject2024/ui/ViewController.kt
@@ -1,28 +1,39 @@
 package com.minseoklim.toyproject2024.ui
 
+import jakarta.servlet.http.HttpServletRequest
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import java.net.URLEncoder
 
 @Controller
-class ViewController {
-
+class ViewController(
+    @Value("\${api-base-url}") private val apiBaseUrl: String
+) {
     @GetMapping("/")
     fun index(): String {
         return "index"
     }
 
     @GetMapping("/login")
-    fun login(): String {
-        return "login"
-    }
-
-    @GetMapping("/join")
-    fun join(): String {
-        return "join"
+    fun login(request: HttpServletRequest): String {
+        return "redirect:$apiBaseUrl/login?redirectUrl=${URLEncoder.encode(getBaseUrl(request), "UTF-8")}"
     }
 
     @GetMapping("/main")
     fun myPage(): String {
         return "main"
+    }
+
+    private fun getBaseUrl(request: HttpServletRequest): String {
+        val scheme = request.scheme
+        val serverName = request.serverName
+        val serverPort = request.serverPort
+
+        return if (serverPort == 80 || serverPort == 443) {
+            "$scheme://$serverName"
+        } else {
+            "$scheme://$serverName:$serverPort"
+        }
     }
 }
