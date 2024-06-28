9c20648739ec143dad579cbb06737d60979fb7f5
minseoklim <ddalgu2@gmail.com>
2024-06-28 11:55:58 +0900

    feat(toy-project-2024-backend:auth): 로그인/회원 가입 화면에 대한 예외 처리
    redirectUrl 쿼리 파라미터가 들어오지 않았을 때에 대한 예외 처리

--- toy-project-2024/backend/auth-module/src/main/kotlin/com/minseoklim/toyproject2024/auth/ui/ViewController.kt
+++ toy-project-2024/backend/auth-module/src/main/kotlin/com/minseoklim/toyproject2024/auth/ui/ViewController.kt
@@ -3,18 +3,27 @@ package com.minseoklim.toyproject2024.auth.ui
 import jakarta.servlet.http.HttpServletRequest
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import java.net.URLEncoder
 
 @Controller
 class ViewController {
 
     @GetMapping("/login")
     fun login(request: HttpServletRequest): String {
-        request.session.setAttribute("REDIRECT_URL", request.getParameter("redirectUrl"))
+        val redirectUrl =
+            request.getParameter("redirectUrl") ?: return redirectWithRedirectUrlParameter(request, "/login")
+        request.session.setAttribute("REDIRECT_URL", redirectUrl)
         return "login"
     }
 
     @GetMapping("/join")
-    fun join(): String {
+    fun join(request: HttpServletRequest): String {
+        request.getParameter("redirectUrl") ?: return redirectWithRedirectUrlParameter(request, "/join")
         return "join"
+    }
+
+    private fun redirectWithRedirectUrlParameter(request: HttpServletRequest, originalPath: String): String {
+        val savedRedirectUrl = request.session.getAttribute("REDIRECT_URL") as String? ?: return "invalidApproach"
+        return "redirect:$originalPath?redirectUrl=${URLEncoder.encode(savedRedirectUrl, "UTF-8")}"
     }
 }
