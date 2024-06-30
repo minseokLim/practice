bda2e6c3a28cc93ed66cbfd012df7c46d89cb66b
minseoklim <ddalgu2@gmail.com>
2024-06-30 16:43:32 +0900

    refactor(toy-project-2024-backend:member): CustomOAuth2UserService을 상속에서 위임으로 구조 변경

--- toy-project-2024/backend/member-module/src/main/kotlin/com/minseoklim/toyproject2024/member/application/CustomOAuth2UserService.kt
+++ toy-project-2024/backend/member-module/src/main/kotlin/com/minseoklim/toyproject2024/member/application/CustomOAuth2UserService.kt
@@ -5,6 +5,7 @@ import com.minseoklim.toyproject2024.member.domain.MemberRepository
 import com.minseoklim.toyproject2024.member.domain.SocialType
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService
 import org.springframework.security.oauth2.core.user.OAuth2User
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -13,9 +14,11 @@ import org.springframework.transaction.annotation.Transactional
 @Transactional
 class CustomOAuth2UserService(
     private val memberRepository: MemberRepository
-) : DefaultOAuth2UserService() {
+) : OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+    private val defaultOAuth2UserService = DefaultOAuth2UserService()
+
     override fun loadUser(userRequest: OAuth2UserRequest): OAuth2User {
-        val attributes = super.loadUser(userRequest).attributes
+        val attributes = defaultOAuth2UserService.loadUser(userRequest).attributes
         val socialType = SocialType.valueOf(userRequest.clientRegistration.registrationId.uppercase())
         val socialId = socialType.extractSocialId(attributes)
 
