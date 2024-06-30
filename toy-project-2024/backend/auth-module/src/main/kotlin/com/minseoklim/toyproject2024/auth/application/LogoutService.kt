ee5650f29482808f01963bad14f8e5855da6890e
minseoklim <ddalgu2@gmail.com>
2024-06-30 15:59:47 +0900

    refactor(toy-project-2024-backend:auth): Logout시 AccessToken에 대한 DB Check 여부를 레디스에 저장하는 로직을 도메인 서비스로 옮김

--- toy-project-2024/backend/auth-module/src/main/kotlin/com/minseoklim/toyproject2024/auth/application/LogoutService.kt
+++ toy-project-2024/backend/auth-module/src/main/kotlin/com/minseoklim/toyproject2024/auth/application/LogoutService.kt
@@ -1,10 +1,8 @@
 package com.minseoklim.toyproject2024.auth.application
 
-import com.minseoklim.toyproject2024.auth.domain.AccessTokenDbCheckFlag
-import com.minseoklim.toyproject2024.auth.domain.AccessTokenDbCheckFlagRepository
+import com.minseoklim.toyproject2024.auth.domain.AccessTokenDbCheckFlagActivator
 import com.minseoklim.toyproject2024.auth.domain.TokenParser
 import com.minseoklim.toyproject2024.auth.dto.TokenRequest
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -12,28 +10,17 @@ import org.springframework.transaction.annotation.Transactional
 @Transactional
 class LogoutService(
     private val deleteTokenService: DeleteTokenService,
-    private val accessTokenDbCheckFlagRepository: AccessTokenDbCheckFlagRepository,
+    private val accessTokenDbCheckFlagActivator: AccessTokenDbCheckFlagActivator,
     private val tokenParser: TokenParser,
-    @Value("\${jwt.access-token-db-check-in-milliseconds}")
-    private val accessTokenDbCheckInMilliseconds: Long
 ) {
     fun logout(request: TokenRequest) {
         deleteTokenService.deleteToken(request.accessToken, request.refreshToken)
-        turnOnAccessTokenDbCheckFlag(extractMemberId(request.accessToken))
+        accessTokenDbCheckFlagActivator.activateAccessTokenDbCheckFlag(extractMemberId(request.accessToken))
     }
 
     fun logoutAll(memberId: Int) {
         deleteTokenService.deleteAllToken(memberId)
-        turnOnAccessTokenDbCheckFlag(memberId)
-    }
-
-    private fun turnOnAccessTokenDbCheckFlag(memberId: Int) {
-        accessTokenDbCheckFlagRepository.save(
-            AccessTokenDbCheckFlag(
-                memberId = memberId,
-                timeToLive = accessTokenDbCheckInMilliseconds
-            )
-        )
+        accessTokenDbCheckFlagActivator.activateAccessTokenDbCheckFlag(memberId)
     }
 
     private fun extractMemberId(accessToken: String): Int {
---
+++ toy-project-2024/backend/auth-module/src/test/kotlin/com/minseoklim/toyproject2024/auth/domain/AccessTokenDbCheckFlagActivatorTest.kt
@@ -0,0 +1,45 @@
+package com.minseoklim.toyproject2024.auth.domain
+
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class AccessTokenDbCheckFlagActivatorTest {
+    private lateinit var accessTokenDbCheckFlagRepository: AccessTokenDbCheckFlagRepository
+    private lateinit var accessTokenDbCheckFlagActivator: AccessTokenDbCheckFlagActivator
+    private val accessTokenDbCheckInMilliseconds = 1000L
+
+    @BeforeEach
+    fun setUp() {
+        accessTokenDbCheckFlagRepository = mockk()
+        accessTokenDbCheckFlagActivator =
+            AccessTokenDbCheckFlagActivator(accessTokenDbCheckFlagRepository, accessTokenDbCheckInMilliseconds)
+    }
+
+    @Test
+    fun activateAccessTokenDbCheckFlag() {
+        // given
+        val memberId = 1
+        every {
+            accessTokenDbCheckFlagRepository.save(any())
+        } returns AccessTokenDbCheckFlag(
+            memberId,
+            accessTokenDbCheckInMilliseconds
+        )
+
+        // when
+        accessTokenDbCheckFlagActivator.activateAccessTokenDbCheckFlag(memberId)
+
+        // then
+        verify {
+            accessTokenDbCheckFlagRepository.save(
+                AccessTokenDbCheckFlag(
+                    memberId,
+                    accessTokenDbCheckInMilliseconds
+                )
+            )
+        }
+    }
+}
