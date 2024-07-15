f5101fdcc1c152ac6877218fdbd37ddb38587e31
minseoklim <ddalgu2@gmail.com>
2024-07-15 17:17:30 +0900

    First commit(websocket-practice)

---
+++ websocket-practice/src/main/java/com/minseoklim/websocketpractice/WebsocketPracticeApplication.java
@@ -0,0 +1,12 @@
+package com.minseoklim.websocketpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsocketPracticeApplication.class, args);
+    }
+}
---
+++ websocket-practice/src/test/java/com/minseoklim/websocketpractice/WebsocketPracticeApplicationTests.java
@@ -0,0 +1,12 @@
+package com.minseoklim.websocketpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
