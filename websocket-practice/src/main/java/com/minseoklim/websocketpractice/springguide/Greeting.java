e38a1c01be94dcef9b137b06b98651a5e583aea0
minseoklim <ddalgu2@gmail.com>
2024-07-15 20:31:38 +0900

    study(websocket-practice)

---
+++ websocket-practice/src/main/java/com/minseoklim/websocketpractice/springguide/Greeting.java
@@ -0,0 +1,12 @@
+package com.minseoklim.websocketpractice.springguide;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class Greeting {
+    private String content;
+}
---
+++ websocket-practice/src/main/java/com/minseoklim/websocketpractice/springguide/HelloMessage.java
@@ -0,0 +1,14 @@
+package com.minseoklim.websocketpractice.springguide;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class HelloMessage {
+    private String name;
+}
