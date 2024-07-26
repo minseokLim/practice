2dbbc563cba71ca5c2a1f143117232c2f9becc8b
minseoklim <ddalgu2@gmail.com>
2024-07-26 17:52:26 +0900

    chore(toy-project-2024-backend): add card-module to app-module

--- toy-project-2024/backend/app-module/build.gradle.kts
+++ toy-project-2024/backend/app-module/build.gradle.kts
@@ -1,6 +1,7 @@
 dependencies {
     // module
     implementation(project(":auth-module"))
+    implementation(project(":card-module"))
     implementation(project(":common-module"))
     implementation(project(":member-module"))
 }
