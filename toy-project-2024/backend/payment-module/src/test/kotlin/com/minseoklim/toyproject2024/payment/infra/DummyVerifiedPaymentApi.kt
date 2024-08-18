82db67a0cb86d824dd05df2654e14d8d3c615ccc
minseoklim <ddalgu2@gmail.com>
2024-08-18 18:30:53 +0900

    test(toy-project-2024-backend:payment): 테스트를 위한 더미 인증 결제 API 컴퍼넌트 생성

---
+++ toy-project-2024/backend/payment-module/src/test/kotlin/com/minseoklim/toyproject2024/payment/infra/DummyVerifiedPaymentApi.kt
@@ -0,0 +1,17 @@
+package com.minseoklim.toyproject2024.payment.infra
+
+import com.minseoklim.toyproject2024.payment.application.VerifiedPaymentApi
+import org.springframework.context.annotation.Primary
+import org.springframework.stereotype.Component
+
+@Primary
+@Component
+class DummyVerifiedPaymentApi : VerifiedPaymentApi {
+    override fun getVerifiedPaymentAmount(paymentUid: String): Long {
+        return 100L
+    }
+
+    override fun cancelPayment(paymentUid: String) {
+        // do nothing
+    }
+}
