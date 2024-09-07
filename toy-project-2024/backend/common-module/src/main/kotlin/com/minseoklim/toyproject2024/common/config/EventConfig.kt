3784e38c0bb885a86c1bba944d83fa3b9c3c37e0
minseoklim <ddalgu2@gmail.com>
2024-09-07 17:19:31 +0900

    chore(toy-project-2024-backend): Async Thread pool 설정

--- toy-project-2024/backend/common-module/src/main/kotlin/com/minseoklim/toyproject2024/common/config/EventConfig.kt
+++ toy-project-2024/backend/common-module/src/main/kotlin/com/minseoklim/toyproject2024/common/config/EventConfig.kt
@@ -3,10 +3,8 @@ package com.minseoklim.toyproject2024.common.config
 import com.minseoklim.toyproject2024.common.util.EventPublisher
 import org.springframework.context.ApplicationContext
 import org.springframework.context.annotation.Configuration
-import org.springframework.scheduling.annotation.EnableAsync
 
 @Configuration
-@EnableAsync
 class EventConfig(
     applicationContext: ApplicationContext
 ) {
