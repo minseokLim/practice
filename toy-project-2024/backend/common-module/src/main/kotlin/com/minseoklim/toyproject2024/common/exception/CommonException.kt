130e641a95f1f7c10914a1faa229ec0cb8a611c0
minseoklim <ddalgu2@gmail.com>
2024-09-12 11:33:05 +0900

    refactor(toy-project-2024-backend): custom exception이 stackTrace 생성하지 않도록 수정

--- toy-project-2024/backend/common-module/src/main/kotlin/com/minseoklim/toyproject2024/common/exception/CommonException.kt
+++ toy-project-2024/backend/common-module/src/main/kotlin/com/minseoklim/toyproject2024/common/exception/CommonException.kt
@@ -3,4 +3,6 @@ package com.minseoklim.toyproject2024.common.exception
 abstract class CommonException(
     val code: String,
     message: String
-) : RuntimeException(message)
+) : RuntimeException(message) {
+    override fun fillInStackTrace(): Throwable = this
+}
