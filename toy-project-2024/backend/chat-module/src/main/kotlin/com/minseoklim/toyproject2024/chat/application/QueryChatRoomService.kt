0e6c94be419930cf20093273cd3c4131e396f3c8
minseoklim <ddalgu2@gmail.com>
2024-10-02 21:37:58 +0900

    refactor(toy-project-2024-backend:chat): 채팅 방 목록 조회 기능 메서드 분리

--- toy-project-2024/backend/chat-module/src/main/kotlin/com/minseoklim/toyproject2024/chat/application/QueryChatRoomService.kt
+++ toy-project-2024/backend/chat-module/src/main/kotlin/com/minseoklim/toyproject2024/chat/application/QueryChatRoomService.kt
@@ -1,10 +1,12 @@
 package com.minseoklim.toyproject2024.chat.application
 
+import com.minseoklim.toyproject2024.chat.domain.model.ChatRoom
 import com.minseoklim.toyproject2024.chat.domain.model.Message
 import com.minseoklim.toyproject2024.chat.domain.repository.ChatRoomRepository
 import com.minseoklim.toyproject2024.chat.domain.repository.MessageRepository
 import com.minseoklim.toyproject2024.chat.dto.application.QueryChatRoomOutput
 import com.minseoklim.toyproject2024.member.application.QueryMemberService
+import com.minseoklim.toyproject2024.member.dto.application.QueryMemberOutput
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -21,16 +23,23 @@ class QueryChatRoomService(
             return emptyList()
         }
 
-        val memberIds = chatRooms.flatMap { it.chatRoomMembers.getMemberIds() }.toSet()
-        val members = queryMemberService.findAllByIds(memberIds)
+        val members = getMembers(chatRooms)
+        val lastMessages = getLastMessages(chatRooms)
 
+        return chatRooms.map { QueryChatRoomOutput.of(it, members, lastMessages) }
+    }
+
+    private fun getMembers(chatRooms: List<ChatRoom>): List<QueryMemberOutput> {
+        val memberIds = chatRooms.flatMap { it.chatRoomMembers.getMemberIds() }.toSet()
+        return queryMemberService.findAllByIds(memberIds)
+    }
+
+    private fun getLastMessages(chatRooms: List<ChatRoom>): List<Message> {
         val lastMessageIds = chatRooms.mapNotNull { it.lastMessageId }
-        val lastMessages: List<Message> = if (lastMessageIds.isEmpty()) {
+        return if (lastMessageIds.isEmpty()) {
             emptyList()
         } else {
             messageRepository.findAllById(lastMessageIds)
         }
-
-        return chatRooms.map { QueryChatRoomOutput.of(it, members, lastMessages) }
     }
 }
