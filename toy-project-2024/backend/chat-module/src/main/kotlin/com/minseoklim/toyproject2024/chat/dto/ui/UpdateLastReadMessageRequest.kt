a06245477988d6c991de5db1202e5c77a9a4fa22
minseoklim <ddalgu2@gmail.com>
2024-10-14 21:42:30 +0900

    refactor(toy-project-2024-backend:chat): UpdateLastReadMessage URL 변경

--- toy-project-2024/backend/chat-module/src/main/kotlin/com/minseoklim/toyproject2024/chat/dto/ui/UpdateLastReadMessageRequest.kt
+++ toy-project-2024/backend/chat-module/src/main/kotlin/com/minseoklim/toyproject2024/chat/dto/ui/UpdateLastReadMessageRequest.kt
@@ -3,10 +3,12 @@ package com.minseoklim.toyproject2024.chat.dto.ui
 import com.minseoklim.toyproject2024.chat.dto.application.UpdateLastReadMessageInput
 
 data class UpdateLastReadMessageRequest(
-    val chatRoomId: Long,
     val lastReadMessageId: Long
 ) {
-    fun toInput(memberId: Int): UpdateLastReadMessageInput {
+    fun toInput(
+        memberId: Int,
+        chatRoomId: Long
+    ): UpdateLastReadMessageInput {
         return UpdateLastReadMessageInput(
             chatRoomId = chatRoomId,
             memberId = memberId,
--- toy-project-2024/backend/chat-module/src/main/kotlin/com/minseoklim/toyproject2024/chat/ui/UpdateLastReadMessageController.kt
+++ toy-project-2024/backend/chat-module/src/main/kotlin/com/minseoklim/toyproject2024/chat/ui/UpdateLastReadMessageController.kt
@@ -3,20 +3,24 @@ package com.minseoklim.toyproject2024.chat.ui
 import com.minseoklim.toyproject2024.chat.application.UpdateLastReadMessageService
 import com.minseoklim.toyproject2024.chat.dto.ui.UpdateLastReadMessageRequest
 import com.minseoklim.toyproject2024.common.annotation.MemberId
+import jakarta.validation.Valid
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class UpdateLastReadMessageController(
     private val updateLastReadMessageService: UpdateLastReadMessageService
 ) {
-    @PostMapping("/chat-rooms/last-read-message")
+    @PostMapping("/chat-rooms/{chatRoomId}/last-read-message")
     fun update(
         @MemberId memberId: Int,
-        request: UpdateLastReadMessageRequest
+        @PathVariable chatRoomId: Long,
+        @Valid @RequestBody request: UpdateLastReadMessageRequest
     ): ResponseEntity<Unit> {
-        updateLastReadMessageService.update(request.toInput(memberId))
+        updateLastReadMessageService.update(request.toInput(memberId, chatRoomId))
         return ResponseEntity.ok().build()
     }
 }
