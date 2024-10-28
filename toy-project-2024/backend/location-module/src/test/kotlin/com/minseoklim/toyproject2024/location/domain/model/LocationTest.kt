d68827d4ba58cd7db8e0592e55b49025bd57c8ab
minseoklim <ddalgu2@gmail.com>
2024-10-28 21:40:41 +0900

    test(toy-project-2024-backend:location): 도메인 테스트 코드 추가

---
+++ toy-project-2024/backend/location-module/src/test/kotlin/com/minseoklim/toyproject2024/location/domain/model/LocationTest.kt
@@ -0,0 +1,49 @@
+package com.minseoklim.toyproject2024.location.domain.model
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.test.util.ReflectionTestUtils
+
+class LocationTest {
+    @Test
+    fun setCoordinate() {
+        // given
+        val location = Location(
+            latitude = 1.0,
+            longitude = 2.0,
+            memberId = 1
+        )
+
+        // when
+        location.setCoordinate(latitude = 3.0, longitude = 4.0)
+
+        // then
+        assertThat(location.coordinate.x).isEqualTo(4.0)
+        assertThat(location.coordinate.y).isEqualTo(3.0)
+    }
+
+    @Test
+    fun equalsAndHashCode() {
+        // given
+        val location1 = Location(
+            latitude = 1.0,
+            longitude = 2.0,
+            memberId = 1
+        )
+        val location2 = Location(
+            latitude = 1.0,
+            longitude = 2.0,
+            memberId = 1
+        )
+        val location3 = Location(
+            latitude = 1.0,
+            longitude = 2.0,
+            memberId = 2
+        )
+        ReflectionTestUtils.setField(location1, "id", 1)
+        ReflectionTestUtils.setField(location2, "id", 1)
+
+        // when, then
+        assertThat(setOf(location1, location2, location3)).hasSize(2)
+    }
+}
