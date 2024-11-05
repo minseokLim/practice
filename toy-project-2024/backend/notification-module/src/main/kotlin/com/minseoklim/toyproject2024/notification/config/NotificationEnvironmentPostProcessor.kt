668a46f17b44b08165bdbdbe3294386fbd7b58f5
minseoklim <ddalgu2@gmail.com>
2024-11-05 21:21:15 +0900

    feat(toy-project-2024-backend:notification): FirebaseApi 수정

--- toy-project-2024/backend/notification-module/src/main/kotlin/com/minseoklim/toyproject2024/notification/config/NotificationEnvironmentPostProcessor.kt
+++
@@ -1,5 +0,0 @@
-package com.minseoklim.toyproject2024.notification.config
-
-import com.minseoklim.toyproject2024.common.config.CustomEnvironmentPostProcessor
-
-class NotificationEnvironmentPostProcessor : CustomEnvironmentPostProcessor("notification")
--- toy-project-2024/backend/notification-module/src/main/kotlin/com/minseoklim/toyproject2024/notification/infra/FirebaseApi.kt
+++ toy-project-2024/backend/notification-module/src/main/kotlin/com/minseoklim/toyproject2024/notification/infra/FirebaseApi.kt
@@ -1,40 +1,93 @@
 package com.minseoklim.toyproject2024.notification.infra
 
 import com.google.auth.oauth2.GoogleCredentials
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
+import com.google.firebase.messaging.AndroidConfig
+import com.google.firebase.messaging.AndroidNotification
+import com.google.firebase.messaging.ApnsConfig
+import com.google.firebase.messaging.ApnsFcmOptions
+import com.google.firebase.messaging.Aps
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.Message
+import com.google.firebase.messaging.Notification
+import com.google.firebase.messaging.WebpushConfig
 import com.minseoklim.toyproject2024.notification.application.PushApi
 import com.minseoklim.toyproject2024.notification.application.PushApi.PushRequest
 import org.springframework.core.io.ClassPathResource
 import org.springframework.stereotype.Component
+import javax.annotation.PostConstruct
 
 @Component
-class FirebaseApi(
-    private val firebaseClient: FirebaseClient
-) : PushApi {
+class FirebaseApi : PushApi {
+    @PostConstruct
+    fun postConstruct() {
+        if (FirebaseApp.getApps().isEmpty()) {
+            val options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(ClassPathResource(FIREBASE_KEY_FILE_PATH).inputStream))
+                .build()
+            FirebaseApp.initializeApp(options)
+        }
+    }
+
     override fun sendPush(request: PushRequest) {
-        val accessToken = "Bearer " + getAccessToken()
-        val requestBody = makeRequestBody(request)
-        firebaseClient.sendMessage(accessToken, requestBody)
+        val message = Message.builder()
+            .setToken(request.token)
+            .setNotification(getNotification(title = request.title, body = request.body, imageUrl = request.imageUrl))
+            .apply {
+                if (request.imageUrl != null) {
+                    setAndroidConfig(getAndroidConfig(request.imageUrl))
+                    setApnsConfig(getApnsConfig(request.imageUrl))
+                    setWebpushConfig(getWebpushConfig(request.imageUrl))
+                }
+            }
+            .build()
+
+        FirebaseMessaging.getInstance().send(message)
     }
 
-    private fun getAccessToken(): String {
-        val googleCredentials = GoogleCredentials.fromStream(ClassPathResource(FIREBASE_KEY_FILE_PATH).inputStream)
-            .createScoped("https://www.googleapis.com/auth/firebase.messaging")
-        googleCredentials.refreshIfExpired()
-        return googleCredentials.accessToken.tokenValue
+    private fun getNotification(
+        title: String,
+        body: String,
+        imageUrl: String?
+    ): Notification {
+        return Notification.builder()
+            .setTitle(title)
+            .setBody(body)
+            .setImage(imageUrl)
+            .build()
     }
 
-    private fun makeRequestBody(request: PushRequest): Map<String, Any?> {
-        return mapOf(
-            "validate_only" to false,
-            "message" to mapOf(
-                "notification" to mapOf(
-                    "title" to request.title,
-                    "body" to request.body,
-                    "image" to request.imageUrl
-                ),
-                "token" to request.token
+    private fun getAndroidConfig(imageUrl: String): AndroidConfig {
+        return AndroidConfig.builder()
+            .setNotification(
+                AndroidNotification.builder()
+                    .setImage(imageUrl)
+                    .build()
             )
-        )
+            .build()
+    }
+
+    private fun getApnsConfig(imageUrl: String): ApnsConfig {
+        return ApnsConfig.builder()
+            .setAps(
+                Aps.builder()
+                    .setContentAvailable(true)
+                    .setMutableContent(true)
+                    .build()
+            )
+            .setFcmOptions(
+                ApnsFcmOptions.builder()
+                    .setImage(imageUrl)
+                    .build()
+            )
+            .build()
+    }
+
+    private fun getWebpushConfig(imageUrl: String): WebpushConfig {
+        return WebpushConfig.builder()
+            .putHeader("image", imageUrl)
+            .build()
     }
 
     companion object {
